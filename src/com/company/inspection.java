ab738637ac475375b45c840a9785f11e45aa07e5
Rithika Sri. J <rithikasri.jayakumar@gmail.com>
2020-11-23 22:28:26 +0530

    inspection class

--- src/com/company/inspection.java
+++ src/com/company/inspection.java
@@ -18,13 +18,6 @@ public class inspection {
                 Connection connection = DriverManager.getConnection(url, user, pw);
                 Statement statement = connection.createStatement()
         ) {
-            String ph_source_id[] = new String[4];
-            String con_source_id[] = new String[3];
-            double invalid_ph[] = new double[4];
-            double high_con[] = new double[3];
-            Date ph_date[] = new Date[4];
-            Date con_date[] = new Date[3];
-
             int con_count = 0;
             int ph_count = 0;
             String query1 = "select * from property order by source_id and inspection_date";
@@ -36,8 +29,8 @@ public class inspection {
             }
             result.close();
             String source_id[]= new String[count];
-            double ph[]= new double[count];
-            double con[]= new double[count];
+            float ph[]= new float[count];
+            float con[]= new float[count];
             Date date[]= new Date[count];
 
             String query2 = "select * from property order by source_id and inspection_date";
@@ -48,14 +41,14 @@ public class inspection {
                 date[i]=result1.getDate("inspection_date");
                 ph[i]=result1.getFloat("ph_level");
                 con[i]=result1.getFloat("contamination_level");
-                System.out.println(source_id[i]+" "+ph[i]+" "+con[i]);
+                System.out.println(source_id[i]+" "+ph[i]+" "+con[i]+" "+date[i]);
             }
 
+           /* for(int j=0;j<count;j++){
 
-            for (int i=0;ph_count>0;ph_count--) {
-                System.out.println(ph_source_id[i]+" "+ph_date[i]+" "+invalid_ph[i]);
-                i++;
-            }
+                ;
+            }*/
+
 
 
 
