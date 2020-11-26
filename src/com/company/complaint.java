06ec53d0909feccdd54bab48293cda46337c7db7
Rithika Sri. J <rithikasri.jayakumar@gmail.com>
2020-11-26 10:25:32 +0530

    wre- view complaints

--- src/com/company/complaint.java
+++ src/com/company/complaint.java
@@ -3,6 +3,11 @@ import java.sql.*;
 import java.util.*;
 
 public class complaint {
+    public String[] location;
+
+    public complaint(){
+        location= new String[]{"Gandhinagar","Siddhapudur","Sungam","Peelamedu","Saibaba Colony"};
+    }
 
     public void assignedtome(String emp_id,String username,String name) throws SQLException {
         String url = "jdbc:mysql://localhost:3306/project_trial";
@@ -14,16 +19,20 @@ public class complaint {
         ) {
             String query1 = "select * from complaints";
             ResultSet result = statement.executeQuery(query1);
+            Random r = new Random();
+
 
             int c=0;
             while(result.next()){
                 String complaint_id=result.getString("complaint_id");
                 String assigned_to=result.getString("assigned_to");
                 String issue=result.getString("issue");
-                if(assigned_to.equals(emp_id) && result.getString("complaint_status").equals("pending")){
-                    c=c+1;
-                    System.out.println("("+c+") "+complaint_id+" "+issue);
-                }
+                int i=0,t=0;
+                    if(assigned_to.equals(emp_id) && result.getString("complaint_status").equals("pending")){
+                        c=c+1;
+                        int random_location= r.nextInt(location.length);
+                        System.out.println("("+c+") "+complaint_id+" "+issue+" @ "+location[random_location]);
+                        }
             }
             result.close();
             if(c==0){
