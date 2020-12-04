c891ed301eeb7fb7dfe0f18bea59788c9d314f50
tarsana <tarsanabalavijayan@gmail.com>
2020-12-04 22:22:33 +0530

    tarsana-admin(add emp method)

--- src/com/company/Public.java
+++ src/com/company/Public.java
@@ -18,6 +18,7 @@ public class Public {
         System.out.print("Welcome back "+(name)+"!");
         System.out.print("\n");
         System.out.println("(1)Report Complaint ");
+        System.out.println("(2)Request New Connection ");
 
         System.out.print("\n\nWhat work do you have? : ");
         int work = input.nextInt();
@@ -26,6 +27,8 @@ public class Public {
             case 1:
                 complaint complaint_obj = new complaint();
                 complaint_obj.report_complaint(username,name);
+                break;
+            case 2:
                 break;
 
 
