0bf8ee8d0e8247fce5478a8f71e8a411240e92d1
sudeepthi <sudeepthimohan24@gmail.com>
2020-12-08 20:56:21 +0530

    saving class payment

--- src/com/company/consumer.java
+++ src/com/company/consumer.java
@@ -1,8 +1,9 @@
 package com.company;
 
 
-import java.sql.*;
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class consumer extends database {
 
@@ -13,7 +14,7 @@ public class consumer extends database {
     }
 
     //checking username existence function:
-    public Boolean check_existing_username() {
+    public boolean check_existing_username() {
         String url = "jdbc:mysql://localhost:3306/project_trial";
         String pw = "n";
         String user = "root";
@@ -27,8 +28,8 @@ public class consumer extends database {
         ) {
 
 
-            Statement statements = connection.createStatement();
-            ResultSet result = statements.executeQuery(query);
+            Statement statement = connection.createStatement()
+            ResultSet result = statement.executeQuery(query);
 
             while (result.next()) {
                 String uname = result.getString("username");
